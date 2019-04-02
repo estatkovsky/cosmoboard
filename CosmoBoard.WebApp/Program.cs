bc7b097e6de24f6f2e983e857eee6ffd1c817722
Evgeny <jekasta@gmail.com>
2019-04-02 15:12:08 +0300

    Configured sentry

--- CosmoBoard.WebApp/Program.cs
+++ CosmoBoard.WebApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace CosmoBoard.WebApp
 {
@@ -12,6 +15,18 @@ namespace CosmoBoard.WebApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration
+                        .MinimumLevel.Information()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        .MinimumLevel.Override("System", LogEventLevel.Warning)
+                        .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                        .Enrich.FromLogContext()
+                        .WriteTo.RollingFile(@"logs/{Date}.log", fileSizeLimitBytes: 1024 * 1024 * 100)
+                        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                        .WriteTo.Sentry("https://e3aafe6ebe0a4e4baaddc3da8edd2d90@sentry.io/1429256");
+                });
     }
 }
